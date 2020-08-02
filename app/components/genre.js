c802152023a307ee00fe5507b5bb8b741dfa056f
AcAyane <45631881+AcAyane@users.noreply.github.com>
2020-08-02 15:50:09 +0000

    Adding filter for each genre

--- app/components/genre.js
+++ app/components/genre.js
@@ -1,21 +1,20 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
-  init() {
-    this._super(...arguments);
-    this.errors = [];
-  },
+  // eslint-disable-next-line ember/require-return-from-computed
+  updateList: computed("model", function () {
+    var genres = ["Adventure", "Action"];
+    for (let i = 0; i < 20; i++) {
+      var filtered = this.model[i].filterBy(
+        (model) => model.get("title") === "The Dark Knight"
+      );
+    }
 
-  didUpdateAttrs() {
-    this._super(...arguments);
-    this.set("errors", []);
-  },
-
-  actions: {
-    required(event) {
-      if (!event.target.value) {
-        this.errors.pushObject({ message: `${event.target.name} is required` });
-      }
-    },
+    // eslint-disable-next-line ember/no-side-effects
+    this.set("model1", filtered);
+  }),
+  didInsertElement() {
+    this.set("model1", this.model);
   },
 });
--- app/controllers/home-page.js
+++ app/controllers/home-page.js
@@ -1,5 +1,18 @@
 import Controller from "@ember/controller";
 
 export default class HomePageController extends Controller {
-  genres = ["Adventure", "action"];
+  genres = [
+    "Adventure",
+    "Action",
+    "Biography",
+    "Drama",
+    "History",
+    "Crime",
+    "Drama",
+    "Thriller",
+    "Family",
+    "Sci-Fi",
+    "Romance",
+    "War",
+  ];
 }
